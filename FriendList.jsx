7a9d15aab5a845fa18d234c9410ef0631c975667
Yash Agarwal <112796695+agarwalyashh@users.noreply.github.com>
2024-02-04 14:27:17 +0530

    Add files via upload

---
+++ FriendList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Friend from "./Friend";
+ export default function FriendsList({ friends, onSelection, selectedFriend }) {
+    return (
+        <ul>
+            {friends.map((friend) => (
+                <Friend
+                    friend={friend}
+                    key={friend.id}
+                    selectedFriend={selectedFriend}
+                    onSelection={onSelection}
+                />
+            ))}
+        </ul>
+    );
+}
